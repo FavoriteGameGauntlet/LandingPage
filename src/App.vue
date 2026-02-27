b060f08a6c7f410a392464eb8f850955b17ed73d
Северьянов Дмитрий <severyanov_da@taximaxim.ru>
2026-02-28 00:12:56 +0500

    Added a header.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
+  <Header/>
 
+  <div class="content">
+    <router-view/>
+  </div>
 
-  <router-view/>
+  <Footer/>
 </template>
 
 <style scoped>
-
+.content {
+  max-width: 1280px;
+  margin: 0 auto;
+  text-align: center;
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="background">
+
+  </div>
+</template>
+
+<style scoped>
+.background {
+  max-width: 1280px;
+  background-color: #0f777a;
+}
+</style>
